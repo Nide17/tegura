0e1ac73b956414dc3137f5d17bf575ac8656ad56
Nide17 <quizblog.rw@gmail.com>
2023-05-15 19:18:35 +0200

    ur student finished

--- lib/screens/auth/ur_student.dart
+++ lib/screens/auth/ur_student.dart
@@ -5,18 +5,20 @@ import "package:tegura/screens/iga/gradient_title.dart";
 import 'package:tegura/screens/utilities/my_appbar.dart';
 import "package:tegura/services/auth.dart";
 
-class Iyandikishe extends StatefulWidget {
-  const Iyandikishe({Key? key}) : super(key: key);
+class UrStudent extends StatefulWidget {
+  const UrStudent({Key? key}) : super(key: key);
 
   @override
-  _IyandikisheState createState() => _IyandikisheState();
+  _UrStudentState createState() => _UrStudentState();
 }
 
 // STATE FOR THE SIGN IN PAGE - STATEFUL
-class _IyandikisheState extends State<Iyandikishe> {
+class _UrStudentState extends State<UrStudent> {
   // AUTH SERVICE INSTANCE
   final AuthService _auth = AuthService();
   final _formKey = GlobalKey<FormState>();
+  String _selectedItem = '';
+  final List<String> _items = ['CST', 'CBE', 'CMHS', 'CE', 'CAVM'];
 
   // BUILD METHOD TO BUILD THE UI OF THE APP
   @override
@@ -35,12 +37,13 @@ class _IyandikisheState extends State<Iyandikishe> {
           children: [
             // 1. GRADIENT TITLE
             GradientTitle(
-                title: 'Iyandikishe', icon: 'assets/images/iyandikishe.svg'),
+                title: 'REGISTER AS UR STUDENT',
+                icon: 'assets/images/ur_student.svg'),
 
             // 2. DESCRIPTION
             const Description(
                 text:
-                    'Iyandikishe ubundi, wige, umenye ndetse utsindire provisoire!'),
+                    'Register as UR student and get a discount up to 50% of regular cost!'),
 
             // CENTERED IMAGE
             Row(
@@ -48,7 +51,7 @@ class _IyandikisheState extends State<Iyandikishe> {
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   Image.asset(
-                    'assets/images/house_keys.png',
+                    'assets/images/50off.png',
                     height: MediaQuery.of(context).size.height * 0.2,
                     width: MediaQuery.of(context).size.width * 0.2,
                   ),
@@ -63,22 +66,82 @@ class _IyandikisheState extends State<Iyandikishe> {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
+                    // FORM DROPDOWN
+                    DropdownButtonFormField<String>(
+                      items: _items.map((String item) {
+                        return DropdownMenuItem<String>(
+                          value: item,
+                          child: Text(
+                            item,
+                            style: const TextStyle(
+                              color: Color(0xFF7FC8DF),
+                            ),
+                          ),
+                        );
+                      }).toList(),
+                      hint: const Text(
+                        'Choose your campus',
+                        style: TextStyle(
+                          color: Color(0xFF7FC8DF),
+                        ),
+                      ),
+                      decoration: const InputDecoration(
+                        // BACKGROUND COLOR
+                        filled: true,
+                        fillColor: Color.fromARGB(255, 255, 255, 255),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(20.0),
+                          ),
+                        ),
+
+                        // HEIGHT
+                        contentPadding: EdgeInsets.symmetric(
+                          vertical: 16.0,
+                          horizontal: 24.0,
+                        ),
+                      ),
+                      onChanged: (String? value) {
+                        setState(() {
+                          _selectedItem = value!;
+                        });
+                      },
+                      value: _selectedItem.isNotEmpty ? _selectedItem : null,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Choose your campus';
+                        }
+                        return null;
+                      },
+                      onSaved: (value) {
+                        // do something with the selected value
+                      },
+                    ),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.02,
+                    ),
                     // NOMERO ZA TELEPHONE
                     DefaultInput(
-                      placeholder: 'Izina',
-                      validation: 'Injiza izina ryawe!',
+                      placeholder: 'Registration number',
+                      validation: 'Please enter your registration number!',
                     ),
 
                     // NOMERO ZA TELEPHONE
                     DefaultInput(
-                      placeholder: 'Nomero za telefone',
-                      validation: 'Injiza numero za telefone!',
+                      placeholder: 'Names',
+                      validation: 'Please enter your names!',
                     ),
 
                     // IJAMBOBANGA
                     DefaultInput(
-                      placeholder: 'Ijambobanga',
-                      validation: 'Injiza ijambobanga!',
+                      placeholder: 'Phone number',
+                      validation: 'Please enter your phone number!',
+                    ),
+
+                    // IJAMBOBANGA
+                    DefaultInput(
+                      placeholder: 'Password',
+                      validation: 'Please enter your password!',
                     ),
 
                     // 4. BUTTON
@@ -97,7 +160,7 @@ class _IyandikisheState extends State<Iyandikishe> {
                           }
                         },
                         child: const Text(
-                          'Iyandikishe',
+                          'Register',
                           textAlign: TextAlign.right,
                           style: TextStyle(
                             color: Color.fromARGB(255, 255, 255, 255),
@@ -111,33 +174,6 @@ class _IyandikisheState extends State<Iyandikishe> {
                       height: MediaQuery.of(context).size.height * 0.035,
                     ),
 
-                    // ROW
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Text(
-                          'Niba wariyandikishije, ',
-                          style: TextStyle(
-                            color: Color.fromARGB(255, 255, 255, 255),
-                          ),
-                        ),
-
-                        // INK WELL
-                        InkWell(
-                          onTap: () {
-                            Navigator.pushReplacementNamed(context, '/injira');
-                          },
-                          child: const Text(
-                            'Injira',
-                            style: TextStyle(
-                              color: Color.fromARGB(255, 0, 27, 116),
-                              decoration: TextDecoration.underline,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-
                     // VERTICAL SPACE
                     SizedBox(
                       height: MediaQuery.of(context).size.height * 0.1,
@@ -148,7 +184,6 @@ class _IyandikisheState extends State<Iyandikishe> {
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         SizedBox(
-                          // width: double.infinity,
                           child: ElevatedButton(
                             style: ElevatedButton.styleFrom(
                                 backgroundColor: const Color(0xFF2C64C6),
@@ -156,28 +191,17 @@ class _IyandikisheState extends State<Iyandikishe> {
                                   borderRadius: BorderRadius.circular(24.0),
                                 )),
                             onPressed: () {
-                              Navigator.pushReplacementNamed(context, '/ur-student');
+                              Navigator.pushReplacementNamed(
+                                  context, '/iyandikishe');
                             },
                             child: const Text(
-                              'Register as UR student',
+                              'Iyandikishe bisanzwe',
                               textAlign: TextAlign.right,
                               style: TextStyle(
                                 color: Color.fromARGB(255, 255, 255, 255),
                               ),
                             ),
                           ),
-                        ),
-
-                        // HORIZONTAL SPACE
-                        SizedBox(
-                          width: MediaQuery.of(context).size.width * 0.02,
-                        ),
-
-                        // PNG IMAGE ASSET
-                        Image.asset(
-                          'assets/images/50off.png',
-                          height: MediaQuery.of(context).size.height * 0.1,
-                          width: MediaQuery.of(context).size.width * 0.1,
                         ),
                       ],
                     ),
